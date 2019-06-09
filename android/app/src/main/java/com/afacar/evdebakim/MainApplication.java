365492c5166500ab1038caa97ac208ff01f1ec74
unknown <acarahmetfaruk@gmail.com>
2019-06-09 15:03:05 +0300

    react-native-voice and native chart installed for android

--- android/app/src/main/java/com/afacar/evdebakim/MainApplication.java
+++ android/app/src/main/java/com/afacar/evdebakim/MainApplication.java
@@ -3,6 +3,7 @@ package com.afacar.evdebakim;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.wenkesj.voice.VoicePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new VoicePackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
