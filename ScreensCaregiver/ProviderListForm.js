7fce31d70027d95d03ced4077597409151fb109d
unknown <acarahmetfaruk@gmail.com>
2019-08-17 17:34:18 +0300

    ProviderCard view is extracted from ProviderList

--- ScreensCaregiver/ProviderListForm.js
+++ ScreensCaregiver/ProviderListForm.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { View, FlatList, Alert } from 'react-native';
-import { Button, Card, Text } from 'react-native-elements';
+import { View, FlatList } from 'react-native';
+import { Text } from 'react-native-elements';
 import { connect } from 'react-redux';
-
-import { RowItem, ColumnItem } from "../components/common/Items";
 import * as actions from '../appstate/actions';
-import { CheckIcon, RequestIcon } from '../components/common/Icons';
+import ProviderCard from '../components/common/ProviderCard';
 
 class ProviderList extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -19,7 +17,7 @@ class ProviderList extends Component {
 
   state = { /** List of provider data */
     providers: {}, // { providerId: {profile} }
-    errorMessages:{}
+    errorMessages: {}
   }
 
   _isMounted = null;
@@ -30,94 +28,10 @@ class ProviderList extends Component {
     );
   }
 
-  _sendProviderRequest = async (providerId, fee) => {
-    try {
-      await this.props.send_provider_request(providerId, fee, (result) => {
-        console.log("Result", result)
-        var errors = this.state.errorMessages;
-        if (result == 'failure')
-          errors[providerId] = "Cüzdanınızda yeterli miktarda para bulunmamaktadır"
-          this.setState({
-            errorMessages: errors
-          })
-      });
-    } catch (error) {
-      console.error('this.props.send_provider_request(providerId) hatası', error.message);
-    }
-  }
-
-  _cancelProviderRequest = async (providerId) => {
-    try {
-      await this.props.cancel_pending_request(providerId);
-    } catch (error) {
-      console.error('this.props.cancel_pending_request(providerId) hatası', error.message);
-    }
-  }
-
-  _cancelRequest = (providerId) => {
-    Alert.alert('Onayınız gerekmekte!', 'Danışmanlık talebini iptal etmek istediğinizden emin misiniz?',
-      [
-        {
-          text: 'İptal',
-          onPress: () => { },
-          style: 'cancel',
-        },
-        { text: 'Evet', onPress: () => this._cancelProviderRequest(providerId) },
-      ],
-      { cancelable: false },
-    );
-  }
-
   _renderItem = ({ item }) => {
     const providerId = item;
-    const provider = this.state.providers[providerId];
-    const content = (
-      <View>
-        <RowItem label="Uzmanlık" content={provider.profession} />
-        <ColumnItem label="Biyografi" content={provider.experience} />
-        <RowItem label="Ücret" content={provider.generalFee ? provider.generalFee : "0"} />
-      </View>
-    );
-    let info = (
-      <Button
-        icon={<RequestIcon color='#041256' />}
-        type="clear"
-        title='Danışmanlık Talep Et'
-        titleStyle={{ color: '#041256' }}
-        onPress={() => this._sendProviderRequest(providerId, provider.generalFee ? provider.generalFee : 0)} />
-    );
-    if (provider.isApproved === true) {
-      info = (
-        <Button
-          disabled
-          title="Danışmanlık Alınıyor"
-          type="clear"
-          icon={<CheckIcon />}
-          disabledTitleStyle={{ color: 'green' }} />);
-    } else if (provider.isApproved === 'pending') {
-      info = (
-        <Button
-          icon={<RequestIcon color='#041256' />}
-          type='clear'
-          title='Onay Bekliyor... Talebi İptal Et!'
-          titleStyle={{ color: '#041256' }}
-          onPress={() => this._cancelRequest(providerId)} />);
-    } else if (provider.isApproved === false) {
-      info = (<Text style={{ color: 'red', fontWeight: 'bold', alignSelf: 'center' }}>Uzman Reddetti!</Text>);
-    } else if (provider.isApproved === 'pause') {
-      info = (<Text style={{ color: 'orange', fontWeight: 'bold', alignSelf: 'center' }}>Hizmet durduruldu!</Text>);
-    }
-    return (
-      <Card
-        title={provider.displayName || 'Adı yok'}
-        titleStyle={{ fontSize: 21 }}
-        containerStyle={{ margin: 3 }}
-      >
-        {content}
-        {info}
-        <Text style={{ color: 'red', fontSize: 12 }}>{this.state.errorMessages ? this.state.errorMessages[providerId] : ""}</Text>
-      </Card>
-    );
+    let provider = { ...this.state.providers[providerId], providerId };
+    return <ProviderCard provider={provider} />
   }
 
   render() {
