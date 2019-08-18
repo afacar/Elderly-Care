6f4d02a7ab7591a2ba1bdfe34c478b900ca6fef6
unknown <acarahmetfaruk@gmail.com>
2019-08-18 21:11:33 +0300

    Settings screens are prettified

--- ScreensProvider/ProviderConsultancySettingsScreen.js
+++ ScreensProvider/ProviderConsultancySettingsScreen.js
@@ -2,35 +2,48 @@ import React, { Component } from 'react';
 import { ScrollView, KeyboardAvoidingView, FlatList } from 'react-native';
 import { SettingsItem } from '../components/common/Items';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ListItem } from 'react-native-elements';
 
 class ProviderConsultancySettingsScreen extends Component {
     static navigationOptions = {
-        title: 'Consultancy Settings',
+        title: 'Danışmanlık Ayarları',
     };
 
     render() {
         return (
-            <ScrollView style={{marginTop: 10}}>
-                    <SettingsItem
-                        key='wallet'
-                        text='Wallet'
-                        onPress={() => this.navigateNextScreen('wallet')}
-                    />
-                    <SettingsItem
-                        key='chat_settings'
-                        text='Chat Settings'
-                        onPress={() => this.navigateNextScreen('chat')}
-                    />
-                    <SettingsItem
-                        key='appointments'
-                        text='Appointments'
-                        // onPress={() => this.navigateNextScreen('profile')}
-                    />
-                    <SettingsItem
-                        key='prelim_questions'
-                        text='Preliminary Questions'
-                        onPress={() => this.navigateNextScreen('prelim_questions')}
-                    />
+            <ScrollView style={{ marginTop: 10 }}>
+                <ListItem
+                    key='wallet'
+                    title='Cuzdan'
+                    onPress={() => this.navigateNextScreen('wallet')}
+                    leftIcon={{ color: '#33cc33', type: 'antdesign', name: 'wallet' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
+                <ListItem
+                    key='chat_settings'
+                    title='Sohbet Ayarları'
+                    onPress={() => this.navigateNextScreen('chat')}
+                    leftIcon={{ type: 'material-community', name: 'message-settings' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
+                <ListItem
+                    key='prelim_questions'
+                    title='Ön Sorular'
+                    onPress={() => this.navigateNextScreen('prelim_questions')}
+                    leftIcon={{ color: '#0066ff', type: 'material-community', name: 'comment-question' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
+                <ListItem
+                    key='appointments'
+                    title='Appointments'
+                    // onPress={() => this.navigateNextScreen('profile')}
+                    leftIcon={{ color: '#cc3300', type: 'material-community', name: 'calendar' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
             </ScrollView >
         )
     }
@@ -38,11 +51,11 @@ class ProviderConsultancySettingsScreen extends Component {
     navigateNextScreen = (screen) => {
         const { navigate } = this.props.navigation;
         if (screen === 'wallet') {
-            navigate('ProviderWalletScreen', {navigation: this.props.navigation});
-        }else if ( screen === 'prelim_questions'){
-            navigate('ProviderPQScreen', {navigation: this.props.navigation});
-        }else if ( screen === 'chat'){
-            navigate('ProviderChatSettingsScreen', {navigation: this.props.navigation});
+            navigate('ProviderWalletScreen', { navigation: this.props.navigation });
+        } else if (screen === 'prelim_questions') {
+            navigate('ProviderPQScreen', { navigation: this.props.navigation });
+        } else if (screen === 'chat') {
+            navigate('ProviderChatSettingsScreen', { navigation: this.props.navigation });
         }
     }
 }
--- ScreensProvider/ProviderSettingsScreen.js
+++ ScreensProvider/ProviderSettingsScreen.js
@@ -2,35 +2,43 @@ import React, { Component } from 'react';
 import { ScrollView, KeyboardAvoidingView, FlatList } from 'react-native';
 import { SettingsItem } from '../components/common/Items';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ListItem } from 'react-native-elements';
 
 class ProviderSettingsScreen extends Component {
     static navigationOptions = {
-        title: 'Settings'
+        title: 'Ayarlar'
     };
 
     render() {
         return (
-            <ScrollView style={{marginTop: 10}}>
-                    <SettingsItem
-                        key='profile'
-                        text='Profile Settings'
-                        onPress={() => this.navigateNextScreen('profile')}
-                    />
-                    <SettingsItem
-                        key='consultancy'
-                        text='Consultancy Settings'
-                        onPress={() => this.navigateNextScreen('consultancy')}
-                    />
-                    <SettingsItem
-                        key='archive'
-                        text='Archive'
-                        onPress={() => this.navigateNextScreen('archive')}
-                    />
-                    <SettingsItem
-                        key='app'
-                        text='App Settings'
-                        onPress={() => this.navigateNextScreen()}
-                    />
+            <ScrollView style={{ marginTop: 10 }}>
+                <ListItem
+                    key='profile'
+                    title='Profil Ayarları'
+                    titleStyle= {{ fontSize: 21 }}
+                    onPress={() => this.navigateNextScreen('profile')}
+                    leftIcon={{ color: '#0066ff', type: 'font-awesome', name: 'user-md' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
+                <ListItem
+                    key='consultancy' 
+                    title='Danışmanlık Ayarları'
+                    titleStyle= {{ fontSize: 21 }}
+                    onPress={() => this.navigateNextScreen('consultancy')}
+                    leftIcon={{ color: '#009933', type: 'material', name: 'settings-phone' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
+                <ListItem
+                    key='archive'
+                    title='Arşiv'
+                    titleStyle= {{ fontSize: 21 }}
+                    onPress={() => this.navigateNextScreen('archive')}
+                    leftIcon={{ color: '#cc3300', type: 'material', name: 'archive' }}
+                    rightIcon={{ type: 'material', name: 'keyboard-arrow-right', size: 33 }}
+                    containerStyle={{ borderBottomWidth: 1, borderBottomLeftRadius: 50 }}
+                />
             </ScrollView >
         )
     }
@@ -38,13 +46,13 @@ class ProviderSettingsScreen extends Component {
     navigateNextScreen = (screen) => {
         const { navigate } = this.props.navigation;
         if (screen === 'profile') {
-            navigate('ProviderProfileScreen', {navigation: this.props.navigation});
+            navigate('ProviderProfileScreen', { navigation: this.props.navigation });
         }
-        else if ( screen === 'consultancy'){
-            navigate('ProviderConsultancySettingsScreen', {navigation: this.props.navigation});
+        else if (screen === 'consultancy') {
+            navigate('ProviderConsultancySettingsScreen', { navigation: this.props.navigation });
         }
-        else if ( screen ==='archive') {
-            navigate('ProviderArchiveScreen', {navigation: this.props.navigation});
+        else if (screen === 'archive') {
+            navigate('ProviderArchiveScreen', { navigation: this.props.navigation });
         }
     }
 }
